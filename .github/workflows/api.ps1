2fc2c84285071e08b9fb0259c2be0f375643a937
swa18141 <127288802+swa18141@users.noreply.github.com>
2023-09-30 11:25:08 +0530

    Create api.ps1

---
+++ .github/workflows/api.ps1
@@ -0,0 +1,17 @@
+# Specify the path to the existing file in your repository
+$filePath = "summary.txt"
+
+
+# Sample data to add to the file
+$sampleData = "created at-- $timestamp`n", "$org`n", "$PROXIES`n","$sharedflowdatastatus`n","$ORGKVMS`n","$apps`n"
+
+Write-Host $sampleData
+
+# Read the existing content of the file
+$fileContent = Get-Content -Path $filePath -Raw
+
+# Append the sample data to the file content
+$newContent = $fileContent + "`n" + $sampleData
+
+# Write the updated content back to the file
+$newContent | Set-Content -Path $filePath
